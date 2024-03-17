c5c6bc62a7be9d644e68c827132c0cbe70e4bc61
Michelet-Gaetan <152990626+Michelet-Gaetan@users.noreply.github.com>
2024-03-17 12:42:42 +0100

    Update visualize.py

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -5,7 +5,7 @@ import json
 
 
 graph = Graph()
-graph.parse("export_test.jsonld", format="json-ld")
+graph.parse("export/export_test.jsonld", format="json-ld")
 
 #You can then navigate the graph using the functions described in https://rdflib.readthedocs.io/en/stable/intro_to_graphs.html
 
